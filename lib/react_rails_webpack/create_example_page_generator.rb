fa086c6c85c379b0c8ff58c46a73edd45246ee3c
Neurodynamic <neurodynamicdev@gmail.com>
2016-08-04 15:25:23 -0400

    fix views folder location

--- lib/react_rails_webpack/create_example_page_generator.rb
+++ lib/react_rails_webpack/create_example_page_generator.rb
@@ -3,17 +3,20 @@ require 'fileutils'
 
 module ReactRailsWebpack
   class CreateExamplePageGenerator < Rails::Generators::Base
+    source_root(File.join(File.expand_path(File.dirname(__FILE__))))
+
     def add_files
       inside 'app' do
         inside 'controllers' do
           copy_file 'react_example_controller.rb'
         end
-      end
 
-      inside 'views' do
-        FileUtils.mkdir_p 'react_example'
-        inside 'react_example' do
-          copy_file 'greeting.rb'
+        inside 'views' do
+          FileUtils.mkdir_p 'react_example'
+
+          inside 'react_example' do
+            copy_file 'greeting.html.erb'
+          end
         end
       end
     end
@@ -21,7 +24,7 @@ module ReactRailsWebpack
     def add_route
       insert_into_file(
         'config/routes.rb',
-        "get 'greeting' => 'react_example#greeting'\n",
+        "  get 'greeting' => 'react_example#greeting'\n",
         after: "Rails.application.routes.draw do\n"
       )
     end
