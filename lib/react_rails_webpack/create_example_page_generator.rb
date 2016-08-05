4ca61c3bf876cc0d91091777191fa6eec4cb33da
Neurodynamic <neurodynamicdev@gmail.com>
2016-08-05 11:50:06 -0400

    add reminders to example generator

--- lib/react_rails_webpack/create_example_page_generator.rb
+++ lib/react_rails_webpack/create_example_page_generator.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 module ReactRailsWebpack
   class CreateExamplePageGenerator < Rails::Generators::Base
     desc "This generator sets up a demo page for react/rails integration in a Rails app."
-    
+
     source_root(File.join(File.expand_path(File.dirname(__FILE__))))
 
     def add_files
@@ -29,6 +29,10 @@ module ReactRailsWebpack
         "  get 'greeting' => 'react_example#greeting'\n",
         after: "Rails.application.routes.draw do\n"
       )
+    end
+
+    def print_next_steps
+      puts "Done! Start your Rails server and go to localhost:3000/greeting to see your React/Rails integration in action!"
     end
   end
 end
