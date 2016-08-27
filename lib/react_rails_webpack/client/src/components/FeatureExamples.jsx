2d6ad5198cfe8d2eed1219b58f670e5167378125
Neurodynamic <neurodynamicdev@gmail.com>
2016-08-27 19:08:06 -0400

    add image and font-awesome examples in FeatureExamples component

---
+++ lib/react_rails_webpack/client/src/components/FeatureExamples.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import 'font-awesome/scss/font-awesome.scss'
+import reactImageUrl from '../images/react_logo.svg'
+
+export default React.createClass({
+  propTypes: {
+    details: React.PropTypes.string.isRequired
+  },
+  getInitialState: function () {
+    return { icon: 'rebel' }
+  },
+  render () {
+    return <div className='bordered-component'>
+      <h2>This is the "FeatureExamples" component from client/src/components/FeatureDemos.jsx</h2>
+
+      <p>Here's an image:</p>
+      <img className='react-logo' src={reactImageUrl} />
+
+      <p>Here are some Font Awesome icons:</p>
+      <i className='fa fa-rebel'></i>
+      <i className='fa fa-empire'></i>
+      <i className='fa fa-first-order'></i>
+      <i className='fa fa-bomb'></i>
+    </div>
+  }
+})
