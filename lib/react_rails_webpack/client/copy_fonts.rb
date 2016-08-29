484bde74031a40dc9afdb20fe1d2319ac00a6a73
Neurodynamic <neurodynamicdev@gmail.com>
2016-08-29 18:44:09 -0400

    add explanatory comment to copy_fonts.rb

--- lib/react_rails_webpack/client/copy_fonts.rb
+++ lib/react_rails_webpack/client/copy_fonts.rb
@@ -1,3 +1,10 @@
+# I can't figure out a cleaner way to get font compilation
+# to work than copying the compiled font files into the public
+# directory. Without this copying step, the font filenames
+# in the Webpack-generated CSS file won't match the filenames
+# of the actual font files. Hoping for a better solution to
+# this in the future.
+
 require 'fileutils'
 
 font_files = Dir.glob('../app/assets/webpack/*.{otf,eot,svg,ttf,woff,woff2}')
